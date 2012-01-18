ee7fae4a4d2a986f860fdbbfe291b3d7a6cbe968
Vadim Shpakovski <vadim@shpakovski.com>
2012-01-18 14:26:33 +0200

    Initial Commit

---
+++ Paging/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Paging
+//
+//  Created by Вадим Шпаковский on 1/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
